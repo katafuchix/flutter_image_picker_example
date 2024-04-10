0618af288cee9fa4b6a4220d99ca2c69e073131d
katafuchix <katafuchix@gmail.com>
2024-04-10 20:36:33 +0900

    lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:video_thumbnail/video_thumbnail.dart';
+import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 
 void main() {
   runApp(MyApp());
+}
+
+
+class MakeThumbnail {
+  Future<String> getThumbnailTwo(String videopath, int position) async {
+    try {
+      final thumbnailFile = await VideoThumbnail.thumbnailFile(
+        video: videopath,
+        timeMs: position,
+        thumbnailPath: (await getTemporaryDirectory()).path,
+        imageFormat: ImageFormat.JPEG,
+      );
+
+      // ignore: unnecessary_null_comparison
+      if (thumbnailFile != null) {
+        return Future<String>.value(thumbnailFile);
+      } else {
+        return Future<String>.value('');
+      }
+    } catch (e) {
+      return Future<String>.value('');
+    }
+    }
 }
 
 class MyApp extends StatelessWidget {
@@ -36,6 +61,8 @@ class _MyHomePageState extends State<MyHomePage> {
       // 画像がnullの場合戻る
       if (image == null) return;
 
+      print(image);
+
       final imageTemp = File(image.path);
 
       setState(() => this.image = imageTemp);
@@ -54,6 +81,19 @@ class _MyHomePageState extends State<MyHomePage> {
       final imageTemp = File(image.path);
 
       setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Future pickVideo() async {
+    try {
+      final file = await ImagePicker().pickVideo(source: ImageSource.gallery);
+      print(file);
+
+// 生成したサムネイルの表示
+
+
     } on PlatformException catch (e) {
       print('Failed to pick image: $e');
     }
@@ -88,6 +128,17 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   onPressed: () {
                     pickImageC();
+                  },
+                ),
+                MaterialButton(
+                  color: Colors.blue,
+                  child: const Text(
+                    "Pick Video from Garary",
+                    style: TextStyle(
+                        color: Colors.white70, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    pickVideo();
                   },
                 ),
                 SizedBox(height: 20),
